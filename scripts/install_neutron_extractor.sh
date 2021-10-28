72eadf44d9c4da781359f93dfdf98d498acf64a0
Nicholas Carrara <ncarrara.physics@gmail.com>
2021-10-28 16:00:25 -0500

    added some setup scripts

--- scripts/install_neutron_extractor.sh
+++ scripts/install_neutron_extractor.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-INSTALL_DIRECTORY   = /dune/app/users/$USER/NeutronCalibrationLArSoft
-LARSOFT_VERSION     = v09_31_00
-DUNETPC_VERSION     = $LARSOFT_VERSION
-ARTG4TK_VERSION     = v10_03_00
-QUALS               = e20:prof
+INSTALL_DIRECTORY=/dune/app/users/$USER/NeutronCalibrationLArSoft
+LARSOFT_VERSION=v09_31_00
+DUNETPC_VERSION=$LARSOFT_VERSION
+ARTG4TK_VERSION=v10_03_00
+QUALS=e20:prof
 
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 setup larsoft $LARSOFT_VERSION -q $QUALS
--- scripts/update_neutron_extractor.sh
+++ scripts/update_neutron_extractor.sh
@@ -11,3 +11,7 @@ cd $INSTALL_DIRECTORY
 source localProducts*/setup
 mrbslp
 
+cd $MRB_SOURCE/dunetpc/dune/NeutronExtractor
+git pull
+
+ninja -C $MRB_BUILDDIR -j 32 install
