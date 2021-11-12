3cc84b5cc84c80bfc0ffe20cf85194e529fcf9db
Nicholas Carrara <ncarrara.physics@gmail.com>
2021-11-12 10:16:42 -0800

    some changes

---
+++ scripts/setup_file_structure.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# set up all of the folder structure
+
+FDSP_FOLDER=$PWD/../FDSP
+1x2x6_FOLDER=$PWD/../FDSP/1x2x6
+mkdir -p FDSP_FOLDER
+mkdir -p 1x2x6_FOLDER
+mkdir -p 1x2x6_FOLDER/hists
+mkdir -p 1x2x6_FOLDER/outputs
+mkdir -p 1x2x6_FOLDER/simple_tests
+mkdir -p 1x2x6_FOLDER/simple_tests/hists
+mkdir -p 1x2x6_FOLDER/simple_tests/outputs
+
+FDVD_FOLDER=$PWD/../FDVD
+1x6x6_FOLDER=$PWD/../FDVD/1x6x6
+mkdir -p FDVD_FOLDER
+mkdir -p 1x6x6_FOLDER
+mkdir -p 1x6x6_FOLDER/hists
+mkdir -p 1x6x6_FOLDER/outputs
+mkdir -p 1x6x6_FOLDER/simple_tests
+mkdir -p 1x6x6_FOLDER/simple_tests/hists
+mkdir -p 1x6x6_FOLDER/simple_tests/outputs
+
+ProtoDUNE-I_FOLDER=$PWD/../ProtoDUNE-I
+DDG_FOLDER=$PWD/../ProtoDUNE-I/DDG
+mkdir -p ProtoDUNE-I_FOLDER
+mkdir -p DDG_FOLDER
+mkdir -p DDG_FOLDER/hists
+mkdir -p DDG_FOLDER/outputs
+mkdir -p DDG_FOLDER/simple_tests
+mkdir -p DDG_FOLDER/simple_tests/hists
+mkdir -p DDG_FOLDER/simple_tests/outputs
