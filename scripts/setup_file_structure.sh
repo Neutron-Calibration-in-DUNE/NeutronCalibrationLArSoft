d903357a4239caa915e35b50e1c0899c6cb8ee98
Nicholas Carrara <ncarrara.physics@gmail.com>
2021-11-12 10:19:42 -0800

    some changes

--- scripts/setup_file_structure.sh
+++ scripts/setup_file_structure.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
 # set up all of the folder structure
 
-FDSP_FOLDER=$PWD/../FDSP
-1x2x6_FOLDER=$PWD/../FDSP/1x2x6
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+NEUTRON_CALIBRATION_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )/../"
+
+mkdir -p NEUTRON_CALIBRATION_DIR/input_files
+
+FDSP_FOLDER=NEUTRON_CALIBRATION_DIR/FDSP
+1x2x6_FOLDER=NEUTRON_CALIBRATION_DIR/FDSP/1x2x6
 mkdir -p FDSP_FOLDER
 mkdir -p 1x2x6_FOLDER
 mkdir -p 1x2x6_FOLDER/hists
@@ -11,8 +21,8 @@ mkdir -p 1x2x6_FOLDER/simple_tests
 mkdir -p 1x2x6_FOLDER/simple_tests/hists
 mkdir -p 1x2x6_FOLDER/simple_tests/outputs
 
-FDVD_FOLDER=$PWD/../FDVD
-1x6x6_FOLDER=$PWD/../FDVD/1x6x6
+FDVD_FOLDER=NEUTRON_CALIBRATION_DIR/FDVD
+1x6x6_FOLDER=NEUTRON_CALIBRATION_DIR/FDVD/1x6x6
 mkdir -p FDVD_FOLDER
 mkdir -p 1x6x6_FOLDER
 mkdir -p 1x6x6_FOLDER/hists
@@ -21,8 +31,8 @@ mkdir -p 1x6x6_FOLDER/simple_tests
 mkdir -p 1x6x6_FOLDER/simple_tests/hists
 mkdir -p 1x6x6_FOLDER/simple_tests/outputs
 
-ProtoDUNE-I_FOLDER=$PWD/../ProtoDUNE-I
-DDG_FOLDER=$PWD/../ProtoDUNE-I/DDG
+ProtoDUNE-I_FOLDER=NEUTRON_CALIBRATION_DIR/ProtoDUNE-I
+DDG_FOLDER=NEUTRON_CALIBRATION_DIR/ProtoDUNE-I/DDG
 mkdir -p ProtoDUNE-I_FOLDER
 mkdir -p DDG_FOLDER
 mkdir -p DDG_FOLDER/hists
