cf09eb4ecdbb960c57157dcbc08d6578057f9138
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-02-23 15:23:17 -0800

    some changes

--- ProtoDUNE-I/UNet/run_generators.sh
+++ ProtoDUNE-I/UNet/run_generators.sh
@@ -4,5 +4,6 @@ mkdir -p DATA_DIR
 
 for i in {0..9}
 do
-    lar -c unet_generator.fcl -o DATA_DIR/generator_$i.root -s ../../dat/ProtoDUNE-I/ProtoDUNE-I_50_1450_$i.dat
+    sed -e "s@REPLACE@$i@" < unet_generator.fcl > temp_generator.fcl
+    lar -c unet_generator.fcl -o DATA_DIR/generator_$i.root 
 done
