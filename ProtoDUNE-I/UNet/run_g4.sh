5e9be5e773d67b1bdd46e7c222f9d8cf96cdbdc2
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-02-23 15:38:31 -0800

    some changes

--- ProtoDUNE-I/UNet/run_g4.sh
+++ ProtoDUNE-I/UNet/run_g4.sh
@@ -4,5 +4,5 @@ mkdir -p $DATA_DIR
 
 for i in {0..9}
 do
-    lar -c unet_g4.fcl -o $DATA_DIR/g4_output.root -s $DATA_DIR/generator_output.root
+    lar -c unet_g4.fcl -o $DATA_DIR/g4_$i.root -s $DATA_DIR/generator_$i.root
 done
--- ProtoDUNE-I/UNet/run_mc_extract.sh
+++ ProtoDUNE-I/UNet/run_mc_extract.sh
@@ -4,5 +4,5 @@ mkdir -p $DATA_DIR
 
 for i in {0..9}
 do
-    lar -c unet_extract.fcl -o $DATA_DIR/extract_output.root -s $DATA_DIR/g4_output.root
+    lar -c unet_extract.fcl -o $DATA_DIR/extract_$i.root -s $DATA_DIR/g4_$i.root
 done
