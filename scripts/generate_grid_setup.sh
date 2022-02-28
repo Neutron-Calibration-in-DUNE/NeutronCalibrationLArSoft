1106c0a60bb94ea3f275ee6426dcf72bc3802e88
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-02-28 10:00:20 -0800

    some changes

---
+++ scripts/generate_grid_setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# get the directory where this script is stored
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+NEUTRON_CALIBRATION_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )/../"
+
+INSTALL_DIRECTORY=/dune/app/users/$USER/NeutronCalibrationInstall
+LARSOFT_VERSION=v09_31_00
+DUNETPC_VERSION=$LARSOFT_VERSION
+ARTG4TK_VERSION=v10_03_00
+QUALS=e20:prof
+
+cp $INSTALL_DIRECTORY/local*/setup $NEUTRON_CALIBRATION_DIR/scripts/setup-grid.sh
+sed -e "s@/dune/app/users/$USER/@${INPUT_TAR_DIR_LOCAL}@" < setup-grid.sh > setup-grid.sh
