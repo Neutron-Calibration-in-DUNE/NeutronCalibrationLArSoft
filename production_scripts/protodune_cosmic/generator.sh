918fb988cc86fb31cc2da49502b4d66ddf41d3cd
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-03-02 07:20:54 -0800

    some changes

--- production_scripts/protodune_cosmic/generator.sh
+++ production_scripts/protodune_cosmic/generator.sh
@@ -6,4 +6,4 @@ lar -n $NUM_EVENTS \
     -c unet_generator.fcl \
     -T $NEUTRON_HISTS/generator_hist_$RUN_NUMBER.root \
     -o $NEUTRON_OUTPUTS/generator_$RUN_NUMBER.root \
-    -s $DATA_DIR/dat/protodune/protodune_50_1450_$RUN_NUMBER.dat
+    -s $NEUTRON_CALIBRATION_DIR/dat/protodune/protodune_50_1450_$RUN_NUMBER.dat
