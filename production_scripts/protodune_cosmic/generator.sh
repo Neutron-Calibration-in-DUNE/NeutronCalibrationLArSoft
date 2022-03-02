1275181e264a303819740d7891e06141a75dac0d
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-03-02 07:29:11 -0800

    some changes

--- production_scripts/protodune_cosmic/generator.sh
+++ production_scripts/protodune_cosmic/generator.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 RUN_NUMBER=0
 NUM_EVENTS=50
+INPUT_FILE=$NEUTRON_CALIBRATION_DIR/dat/protodune/protodune_50_1450_$RUN_NUMBER.dat
+
+sed -e "s@REPLACE@$INPUT_FILE@" < $PROTODUNE_UNET_DIR/unet_generator.fcl > $PROTODUNE_UNET_DIR/temp_generator_$RUN_NUMBER.fcl
 
 lar -n $NUM_EVENTS \
-    -c unet_generator.fcl \
+    -c temp_generator_$RUN_NUMBER.fcl \
     -T $NEUTRON_HISTS/generator_hist_$RUN_NUMBER.root \
-    -o $NEUTRON_OUTPUTS/generator_$RUN_NUMBER.root \
-    -s $NEUTRON_CALIBRATION_DIR/dat/protodune/protodune_50_1450_$RUN_NUMBER.dat
+    -o $NEUTRON_OUTPUTS/generator_$RUN_NUMBER.root
